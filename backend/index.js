7289487a9f3eb6da54be3bd2537239a8c466e9ab
AshanMaduwantha <153969463+AshanMaduwantha@users.noreply.github.com>
2024-07-07 19:03:46 +0530

    modified codes

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,9 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//Databse Connection With MongoDB
+mongoose.connect("mongodb+srv://ashan23:ashan23@testing.2npte0k.mongodb.net/Testing");
