1a0b58d5fc0db0814349d5c1d73ad6f9a540757c
AshanMaduwantha <153969463+AshanMaduwantha@users.noreply.github.com>
2024-07-09 11:37:14 +0530

    modified codes

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../Assets/nav-logo.svg'
+import navprofileIcon from '../Assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={navlogo} className='nav-logo' alt="" />
+      <img src={navprofileIcon} className='nav-profile' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
